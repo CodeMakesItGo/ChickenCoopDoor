eb24beaf414cbc7343562222ce4614b9de0f2826
CodeMakesItGo <jason.altice@codemakesitgo.com>
2021-09-18 16:40:04 -0600

    Updated Connections
    Fixed brown out condition when reversing actuator direction

--- chicken_coop/chicken_coop.ino
+++ chicken_coop/chicken_coop.ino
@@ -2,18 +2,23 @@
  * Uses an Arduino Nano
  * to interface to a smart garage door controller 
  * Rev1.1 removed the need for Sync
+ * Rev1.2 Fixed issue with reversing actuator direction
  */
-#define VERSION "Chicken Coop Door 1.1\n"
+#define VERSION "Chicken Coop Door 1.2\n"
 
 //Pin assignments
 #define TOGGLER_INPUT PD2 
-#define CLOSED_SENSOR PD3 
-#define MOTOR_CONTROL PD4 
+#define CLOSED_SENSOR_D PD3 
+#define MOTOR_CONTROL_1 PD4 
 #define LED_OUTPUT PD5 
 #define DOOR_STATUS_OUTPUT PD6
+#define MOTOR_CONTROL_2 PD7 
+#define CLOSED_SENSOR_A A1
+
 
 #define DEBOUNCE    1500  // 1500 milli second
 #define BLINK_RATE  1000  // 1 second blink rate
+#define MOTOR_DELAY 250   // Short delay so motor doesn't cause voltage drop
 
 static bool door_open = false;     // Motor output state of door
 
@@ -22,39 +27,43 @@ void setup()
   //Pin Outputs
   pinMode(DOOR_STATUS_OUTPUT, OUTPUT);
   pinMode(LED_OUTPUT, OUTPUT);
-  pinMode(MOTOR_CONTROL, OUTPUT);
+  pinMode(MOTOR_CONTROL_1, OUTPUT);
+  pinMode(MOTOR_CONTROL_2, OUTPUT);
   digitalWrite(LED_OUTPUT, HIGH);
-  digitalWrite(MOTOR_CONTROL, HIGH);
+  digitalWrite(MOTOR_CONTROL_1, LOW);
+  digitalWrite(MOTOR_CONTROL_2, LOW);
   
   //Pin Inputs 
   pinMode(TOGGLER_INPUT, INPUT_PULLUP);
-  pinMode(CLOSED_SENSOR, INPUT_PULLUP);
+  pinMode(CLOSED_SENSOR_D, INPUT);
+  pinMode(CLOSED_SENSOR_A, INPUT);
   
   // Start the Serial comms
   Serial.begin(9600);         
   delay(10);
   Serial.println(VERSION);
 
-  //Delay 2 seconds for the garage sensor to start
-  //Actuator will start to close the door for 2 seconds if it is open
-  // but will switch back to open within this time. This is to midigate if
-  // there is a reset while the door is closed. 
-  delay(2000);
+  //Delay 1 seconds for the garage sensor to start
+  delay(1000);
  
   // on startup, if door is closed then initilaize the motor to close
-  if(digitalRead(CLOSED_SENSOR) == LOW)
+  if(digitalRead(CLOSED_SENSOR_D) == LOW)
   {
-    digitalWrite(MOTOR_CONTROL, HIGH);
+    digitalWrite(MOTOR_CONTROL_1, HIGH);
+    delay(MOTOR_DELAY);
+    digitalWrite(MOTOR_CONTROL_2, HIGH);
     digitalWrite(DOOR_STATUS_OUTPUT, HIGH);
     door_open = false;
-    Serial.println("Door is CLOSED");
+    Serial.println("Door is OPEN");
   }
   else
   {
-    digitalWrite(MOTOR_CONTROL, LOW);
+    digitalWrite(MOTOR_CONTROL_1, LOW);
+    delay(MOTOR_DELAY);
+    digitalWrite(MOTOR_CONTROL_2, LOW);
     digitalWrite(DOOR_STATUS_OUTPUT, LOW);
     door_open = true;
-    Serial.println("Door is OPEN");
+    Serial.println("Door is CLOSED");
   }
 }
 
@@ -69,8 +78,11 @@ void blink_led()
     blink = !blink;
     digitalWrite(LED_OUTPUT, blink ? HIGH : LOW);
 
+    //Analog input read of the door sensor (debug)
+    Serial.println(analogRead(CLOSED_SENSOR_A));
+
     //indicate door state
-    if(digitalRead(CLOSED_SENSOR) == LOW)
+    if(digitalRead(CLOSED_SENSOR_D) == HIGH)
     {
       digitalWrite(DOOR_STATUS_OUTPUT, HIGH);
       Serial.println("Door is CLOSED");
@@ -109,7 +121,9 @@ void toggle_door()
 {
   door_open = !door_open;
   
-  digitalWrite(MOTOR_CONTROL, door_open ? LOW : HIGH);
+  digitalWrite(MOTOR_CONTROL_1, door_open ? LOW : HIGH);
+  delay(MOTOR_DELAY); 
+  digitalWrite(MOTOR_CONTROL_2, door_open ? LOW : HIGH);
   
   Serial.println("Door is " + String(door_open ? "opening" : "closing"));
 }
